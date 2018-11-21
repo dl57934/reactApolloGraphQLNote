022a6656a3fcee645bcfbd2b94f9d70dc10e11db
dl57934 <dl57934@pukyong.ac.kr>
2018-11-22 04:53:15 +0900

    complete 1.3

---
+++ READ.md
@@ -0,0 +1,7 @@
+#offline Note APP
+
+offline first APP MARK DOWN NOTE APP built app
+
+설치한 것들
+
+apollo-cache-inmemory react-apollo apollo-boost apollo-client graphql graphql-tag styled-components styled-reset react-textarea-autosize apollo-link-state
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import GlobalStyled from "./GlobalStyled";
+import client from "./apollo";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyled />
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
